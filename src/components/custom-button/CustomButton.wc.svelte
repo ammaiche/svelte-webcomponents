f3b6cdc59bea129835123c27f46536ad54326028
mahdi.maiche <mahdi.maiche@contentsquare.com>
2021-02-22 15:42:23 +0100

    [7] fix event issue by dispatching a javascript event

--- src/components/custom-button/CustomButton.wc.svelte
+++ src/components/custom-button/CustomButton.wc.svelte
@@ -1,26 +1,23 @@
-
-<svelte:options tag="custom-button" />
 <script lang="ts">
-  import { createEventDispatcher } from 'svelte';
-
-	const dispatch = createEventDispatcher();
-
-
   export let myDescription = "Custom button";
 
   function onButtonClick() {
-   dispatch('customClick');
+    const event = new CustomEvent("customClick", {
+      composed: true, // makes the event jump shadow DOM boundary
+    });
+
+    this.dispatchEvent(event);
   }
 </script>
 
 <style lang="scss">
   @import "./CustomButton.scss";
-  .title{
+  .title {
     color: blue;
   }
 </style>
 
-<template>
+<svelte:options tag="custom-button" /><template>
   <csm-button primary on:csmClick={onButtonClick}>
     <div class="title">
       <slot />
